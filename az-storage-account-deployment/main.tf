a9a14dead0af54741cc3239b2b763ee314b7a7d1
acmarpu <acmarpu@gmail.com>
2023-02-05 12:04:44 +0530

    azure-pipeline updated

--- az-storage-account-deployment/main.tf
+++ az-storage-account-deployment/main.tf
@@ -1,3 +1,4 @@
+# terraform required_providers setion is optional here
 terraform {
   required_providers {
     azurerm = {
@@ -8,12 +9,13 @@ terraform {
 }
 
 provider "azurerm" {
+    version = "=2.13.0"
     features {
       
     }
   
 }
-
+# backend azure storage account details 
 terraform {
   backend "azurerm" {
     resource_group_name  = "StorageAccount-ResourceGroup"
@@ -23,4 +25,24 @@ terraform {
   }
 }
 
+#Use this data source to access the configuration of the Azure Resource Manager provider.
 data "azurerm_client_config" "current" {}
+
+
+# below code for azure storage account details
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
